a9adeee1e4ff406ea020af705484f4ad04c86447
Vlad Gerasimovich <hotepp@icloud.com>
2023-02-02 14:54:19 +0100

    feat: create object of ip arrays, delete key if more than 10

--- solutions/clicks/index.js
+++ solutions/clicks/index.js
@@ -1,3 +1,22 @@
+// const isSameHour = require("date-fns/isSameHour");
 const data = require("./data/index.json");
 
-console.log(data);
+const ips = {};
+
+data.forEach((click) => {
+  const { ip } = click;
+
+  if (!ips[ip]) {
+    ips[ip] = [];
+  }
+
+  ips[ip].push(click);
+});
+
+Object.keys(ips).forEach((key) => {
+  if (ips[key].length > 10) {
+    delete ips[key];
+  }
+});
+
+// console.log(ips);
