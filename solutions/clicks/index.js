ffdafeb3f9381c88082c077f79d95bdb4d25a15f
Vlad Gerasimovich <hotepp@icloud.com>
2023-02-03 10:05:00 +0100

    feat: add comments to clicks/index.js

--- solutions/clicks/index.js
+++ solutions/clicks/index.js
@@ -3,17 +3,25 @@ const isSameHour = require("date-fns/isSameHour");
 const parse = require("date-fns/parse");
 const data = require("./data/index.json");
 
-const ips = {};
+const clicksGroupedByIP = {};
 const result = [];
 
-const getGreatestAmountInSameHour = (clickOne, clickTwo) => {
+/**
+ * I assumed this is the format used in the original data (day first, month second)
+ * If we need to change it, it'd be as simple as updating this variable
+ */
+const timestampFormat = "d/M/y kk:mm:ss";
+
+const getPriorityClick = (clickOne, clickTwo) => {
   const { timestamp: timestampOne, amount: amountOne } = clickOne;
   const { timestamp: timestampTwo, amount: amountTwo } = clickTwo;
 
-  const clickDateOne = parse(timestampOne, "d/M/y kk:mm:ss", new Date());
-  const clickDateTwo = parse(timestampTwo, "d/M/y kk:mm:ss", new Date());
+  const clickDateOne = parse(timestampOne, timestampFormat, new Date());
+  const clickDateTwo = parse(timestampTwo, timestampFormat, new Date());
 
+  // Check if both clicks are in the same hour
   if (isSameHour(clickDateOne, clickDateTwo)) {
+    // Return the earliest click, if amounts are equal
     if (amountOne === amountTwo) {
       if (isBefore(clickDateOne, clickDateTwo)) {
         return clickOne;
@@ -22,6 +30,7 @@ const getGreatestAmountInSameHour = (clickOne, clickTwo) => {
       return clickTwo;
     }
 
+    // Return the click with the greatest amount
     if (amountOne > amountTwo) {
       return clickOne;
     }
@@ -29,52 +38,76 @@ const getGreatestAmountInSameHour = (clickOne, clickTwo) => {
     return clickTwo;
   }
 
+  // Return null to tell, that clicks are not in the same hour
   return null;
 };
 
+// Fill clicksGroupedByIP object for better data filtering/manipulation
 data.forEach((click) => {
   const { ip } = click;
 
-  if (!ips[ip]) {
-    ips[ip] = [];
+  if (!clicksGroupedByIP[ip]) {
+    clicksGroupedByIP[ip] = [];
   }
 
-  ips[ip].push(click);
+  clicksGroupedByIP[ip].push(click);
 });
 
-Object.keys(ips).forEach((key) => {
-  const clicks = ips[key];
+// Go through every unique IP individually
+Object.keys(clicksGroupedByIP).forEach((key) => {
+  const clicks = clicksGroupedByIP[key];
   const { length } = clicks;
 
+  // If IP has more than 10 clicks — delete this IP with its clicks collection from object
   if (length > 10) {
-    delete ips[key];
+    delete clicksGroupedByIP[key];
   } else {
+    // Go throgh every click of current IP
     clicks.forEach((click, index) => {
-      if (length > 1) {
-        const lengthArray = [...Array(length).keys()];
+      // If we only have one click — push it to result right away
+      if (length === 1) {
+        result.push(click);
+      } else {
         const lastIndex = length - 1;
-        let theGreatest = click;
-        let theGreatestIndex = index;
+        let priorityClick = click;
+        let priorityClickIndex = index;
 
-        lengthArray.forEach((i) => {
-          if (theGreatestIndex !== i) {
-            const theGreatestFromTwo = getGreatestAmountInSameHour(
-              theGreatest,
-              clicks[i]
+        // Reiterate through clicks of current IP to define priority clicks for result
+        clicks.forEach((reiteratedClick, reiteratedIndex) => {
+          // Skip comparing the same clicks
+          if (priorityClickIndex !== reiteratedIndex) {
+            // Define priority click by comparing two
+            const definedPriorityClick = getPriorityClick(
+              priorityClick,
+              clicks[reiteratedIndex]
             );
 
-            if (theGreatestFromTwo && theGreatestFromTwo !== theGreatest) {
-              theGreatest = theGreatestFromTwo;
-              theGreatestIndex = i;
-            } else if (lastIndex === i || lastIndex === index) {
-              if (!result.find((resultClick) => resultClick === theGreatest)) {
-                result.push(theGreatest);
+            /**
+             * Update priority click, if it was defined and changed from before
+             *
+             * Continue checking every other click until the last one
+             * in case that we have the same hour later on in the clicks collection
+             */
+            if (
+              definedPriorityClick &&
+              definedPriorityClick !== priorityClick
+            ) {
+              priorityClick = definedPriorityClick;
+              priorityClickIndex = reiteratedIndex;
+            } else if (lastIndex === reiteratedIndex || lastIndex === index) {
+              /**
+               * If it is the last click in either of iterations (original or reiteration)
+               * and if we don't already have this click in result
+               * push it to result
+               */
+              if (
+                !result.find((resultClick) => resultClick === priorityClick)
+              ) {
+                result.push(priorityClick);
               }
             }
           }
         });
-      } else {
-        result.push(click);
       }
     });
   }
