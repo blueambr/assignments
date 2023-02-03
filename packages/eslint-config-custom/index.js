608da6ae65dfe735518c312d2f90e97ec086237d
Vlad Gerasimovich <hotepp@icloud.com>
2023-02-03 16:55:49 +0100

    feat: update config files, add vitest, add suite.test.js example

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,3 +1,7 @@
 module.exports = {
   extends: ["airbnb-base", "turbo", "prettier"],
+  rules: {
+    "import/no-extraneous-dependencies": "off",
+    "no-console": ["warn", { allow: ["error", "info", "warn"] }],
+  },
 };
--- solutions/clicks/.eslintrc.js
+++ solutions/clicks/.eslintrc.js
@@ -1,8 +1,4 @@
 module.exports = {
   root: true,
   extends: ["custom"],
-  rules: {
-    "import/no-extraneous-dependencies": 0,
-    "no-console": ["warn", { allow: ["error", "info", "warn"] }],
-  },
 };
